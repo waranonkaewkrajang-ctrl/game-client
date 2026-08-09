883bb2e5522d267502a79f0158e97dce6427e5de
waranonkaewkrajang-ctrl <waranonkaewkrajang@gmail.com>
2026-08-09 15:16:40 +0700

    เปิด standalone mode

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   compress: true,
+  output: "standalone",
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '**' },
